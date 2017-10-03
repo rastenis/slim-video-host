a0e0577c45a9eb59884ba91124b194ad3d0da30a
Matas R <matas2k@gmail.com>
2017-10-03 18:06:50 +0300

    Switched from mong-portable to nedb as primary storage [currently non-functional, nedb not fully implemented yet]

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
+//deps
 require('dotenv').config();
-const MongoPortable = require("mongo-portable").MongoPortable;
+process.env.DEBUG = 'nuxt:*'
 const bcrypt = require('bcrypt');
 const shortid = require('shortid');
 const chalk = require('chalk');
-var FileSystemStore = require("file-system-store");
-
-//setupinam databasus
-var db = new MongoPortable("TEST");
-db.addStore(FileSystemStore);
-
-var adminRegistered = checkAdminReg();
-console.log("admin reg: " + adminRegistered);
-
-process.env.DEBUG = 'nuxt:*'
-
+var Datastore = require('nedb');
 const { Nuxt, Builder } = require('nuxt');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = require('express')();
 
-var defaultStorageSpace = 10; //gigabaitais
+//uzkraunam DB
+db = {};
+db.users = new Datastore({ filename: 'db/users', autoload: true });
+db.codes = new Datastore({ filename: 'db/codes', autoload: true });
+db.videos = new Datastore({ filename: 'db/videos', autoload: true });
 
-// Body parser, to access req.body
+const defaultUserStatus = 0; //1 - admin
+const defaultStorageSpace = 10; //gigabaitais
+
+//skirta isjungti admin registered flagui, kad tik pirma useri padarytu automatiskai adminu.
+var adminRegistered = checkAdminReg();
+
+
 app.use(bodyParser.json());
-
-// Sessions to create req.session
 app.use(session({
     secret: 'super-secret-key',
     resave: false,
@@ -35,63 +34,86 @@ app.use(session({
 // postas loginui. Reikalingas, kad butu pasiekiama $store.state.authUser
 app.post('/api/login', function(req, res) {
 
-    //bcrypt.compareSync(pass, hash);
-    var users = db.collection("users");
-    var cursor = users.find({ username: req.body.username.toLowerCase() });
+    db.users.find({ username: req.body.username.toLowerCase() }, function(err, docs) {
 
-    let count = 0;
-    cursor.forEach(function(doc) { //jei randa daugiau nei 1 - problem
-        console.log(chalk.bgGreen("ELEMENT: " + doc));
-        if (count >= 1) { //checkas del butent dupe username accounts (NETURETU TOKIU BUT EVER)
-            console.log(chalk.bgRed.white("== ACCOUNTS WITH MATCHING USERNAMES DETECTED =="));
-        } else {
-            if (bcrypt.compareSync(req.body.password, doc.password)) { //passwords match
-                console.log(chalk.green("passwords match!"));
-                req.session.authUser = doc;
+        let count = 0;
+        docs.forEach(function(doc) { //jei randa daugiau nei 1 - problem
+            console.log(chalk.bgGreen("ELEMENT: " + doc.username));
+            if (count >= 1) { //checkas del butent dupe username accounts (NETURETU TOKIU BUT EVER)
+                console.log(chalk.bgRed.white("== ACCOUNTS WITH MATCHING USERNAMES DETECTED =="));
             } else {
-                console.log(chalk.red("passwords don't match!"));
-                res.status(401).json({ error: 'Bad credentials' });
+                if (bcrypt.compareSync(req.body.password, doc.password)) { //passwords match
+                    console.log(chalk.green("passwords match!"));
+                    req.session.authUser = doc;
+                    return res.json(doc);
+                } else {
+                    console.log(chalk.red("passwords don't match!"));
+                    res.status(401).json({ error: 'Bad credentials' });
+                }
             }
-        }
-        count++;
-    });
+            count++;
+        });
 
-    if (count == 0) {
-        res.status(402).json({ error: 'No user with those credentials found.' });
-    }
+        if (count == 0) {
+            res.status(402).json({ error: 'No user with those credentials found.' });
+        }
+    });
 
 });
 
 app.post('/api/register', function(req, res) {
 
-    var users = db.collection("users");
-    var codes = db.collection("codes"); //skirta reg kodams sutikrinti ir duot atitinkamai vietos
-    var cursor = users.find({});
-    //checkai del duplicate users
-    if (checkForDuplicateUN(req.body.username, cursor)) { // bando prisiregint dupe username. throw back
-        res.status(401).json({ error: 'An account with that username already exists.' });
-    } else { //ok, dedam i DB ir prikabinam prie session kad nereiktu loginintis
-        var storageSpace = defaultStorageSpace;
-        var defUserStatus = 0;
-        var codeCursor = codes.find({ code: req.body.code });
+    console.log(chalk.bgCyanBright("OK "));
 
-        if (resolveCode(codeCursor)) { //code found
-            storageSpace = resolveCode(codeCursor);
+    db.users.find({ username: req.body.username.toLowerCase() }, function(err, docs) {
+
+        //checkai del duplicate usernames
+        if (Array.isArray(docs) || docs.length) { //rado useri su tokiu paciu username
+            res.status(401).json({ error: 'An account with that username already exists.' });
+            //TODO: add handle for this in vuex
+        } else { //ok, dedam i DB ir prikabinam prie session kad nereiktu loginintis
+            var storageSpace = defaultStorageSpace;
+            var userStatus = defaultUserStatus;
+
+            async.waterfall([
+                function(callback) { //tikrinimas ar yra atitinkanciu privelegiju kodu
+                    callback(null, null);
+                    db.codes.find({ code: req.body.code }, function(err, docs) {
+                        if (!Array.isArray(docs) || !docs.length) { //rado useri su tokiu paciu username
+                            //no matching code, go on
+                            callback(null, null);
+                        } else {
+                            //got a matching code!
+                            //TODO?: remove code from DB after activation maybe or maybe not
+                            callback(null, docs[0]);
+                        }
+                    });
+                },
+                function(code, callback) {
+                    if (code !== null) { //got code
+                        //TODO: code logic, padidint duomenu kieki OR statusa pakeist
+                    }
+                    if (!adminRegistered) {
+                        userStatus = 1; //duodam pirmam useriui admin statusa
+                    }
+
+                    var hashedPass = hashUpPass(req.body.password);
+
+                    db.users.insert({ username: req.body.username.toLowerCase(), password: hashedPass, email: req.body.email, totalSpace: storageSpace, userStatus: userStatus }, function(err, doc) {
+                        console.log(chalk.bgCyanBright("successfully inserted user " + doc.username));
+                        req.session.authUser = doc; //kabinam visa user ant authUser
+                        return res.json(doc);
+                        callback(null); //shouldnt be reached siaip
+                    });
+
+                }
+            ], function(err, res) {
+                if (err) {
+                    console.log(chalk.bgRed.white(err));
+                }
+            });
         }
-        if (!adminRegistered) {
-            defUserStatus = 1; //duodam pirmam useriui admin statusa
-        }
-
-        var hashedPass = hashUpPass(req.body.password);
-        console.log("hashed to " + hashedPass);
-
-        var newUser = users.insert({ username: req.body.username.toLowerCase(), password: hashedPass, email: req.body.email, totalSpace: storageSpace, userStatus: defUserStatus });
-
-        req.session.authUser = newUser; //kabinam visa user ant authUser
-        return res.json(newUser);
-    }
-
-
+    });
 });
 
 // removinam useri is req.session on logout
@@ -118,51 +140,26 @@ console.log('Server is listening on http://localhost:10700');
 
 
 function checkAdminReg() {
-    var users = db.collection("users");
-    var cursor = users.find({});
 
-    var userCount = 0;
-    cursor.forEach(function(doc) {
-        console.log("users found = " + doc);
-        userCount++;
+    db.users.find({}, function(err, docs) {
+        var userCount = 0;
+        docs.forEach(function(doc) {
+            console.log("user found = " + doc.username);
+            userCount++;
+        });
+
+        if (userCount == 0) { //ADMINAS DAR NEPRISIREGISTRAVES; settinam admin flag to false
+            return false;
+        } else {
+            return true;
+        }
+
+        console.log("admin reg: " + adminRegistered);
     });
-
-    if (userCount == 0) { //ADMINAS DAR NEPRISIREGISTRAVES; settinam admin flag to false
-        return false;
-    } else {
-        return true;
-    }
 }
 
-function resolveCode(cursor) {
-    cursor.forEach(function(doc) {
-        return doc.space;
-    });
-    return false;
-}
 
 function hashUpPass(pass) {
     var hash = bcrypt.hashSync(pass, 10);
     return hash;
-}
-
-function checkForDuplicateUN(username, cursor) {
-    cursor.forEach(function(doc) {
-        console.log("checking username against " + doc.username);
-        if (doc.username.toLowerCase() == username.toLowerCase()) { //konvertuojam kad butu case insensitive usernames
-            //dupe found
-            return true;
-        }
-    });
-    return false;
-}
-
-function isJSON(data) {
-    var ret = true;
-    try {
-        JSON.parse(data);
-    } catch (e) {
-        ret = false;
-    }
-    return ret;
 }
