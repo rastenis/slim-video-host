5ba1ba3be97e308eaef05cdabf0ce8b897153dc7
Matas R <matas2k@gmail.com>
2017-12-20 17:14:13 +0200

    reverted vue-chart integration - doesn't play well with nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,11 @@
 module.exports = {
     build: {
         analyze: false,
-        vendor: ['element-ui', 'axios', 'vue-clipboard2', 'vue-charts']
+        vendor: ['element-ui', 'axios', 'vue-clipboard2']
     },
     plugins: [
         '~plugins/element-ui',
-        '~plugins/vue-clipboard2',
-        '~plugins/vue-charts'
+        '~plugins/vue-clipboard2'
     ],
     css: [
         'element-ui/lib/theme-chalk/index.css',
--- plugins/vue-charts.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import vueCharts from 'vue-charts'
-
-Vue.use(vueCharts);
