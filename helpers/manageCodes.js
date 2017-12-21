499d7ebf211178ac9cbb4da83dcb518ce934ea2f
Matas R <matas2k@gmail.com>
2017-12-21 12:44:58 +0200

    added admin status code genertion into the code tool, cleaned it up a bit

--- helpers/manageCodes.js
+++ helpers/manageCodes.js
@@ -8,40 +8,54 @@ db.codes = new Datastore({
     autoload: true
 });
 
-var end = false;
-while (!end) {
+mainLoop: while (true) {
     console.log(chalk.bgCyan.black("========CODE MANAGER========"));
-    console.log("1. Add code");
-    console.log("2. Remove code");
-    console.log("3. Update 'active' state of a specific code");
+    console.log("1. Add code for storage space");
+    console.log("2. Add code for admin status");
+    console.log("3. Remove code");
+    console.log("4. Update 'active' state of a specific code");
     console.log("ENTER. Exit.");
 
     var choice = prompt('choose:');
 
-
     switch (choice) {
         case "1":
             var code = prompt('enter code:');
-            var space = prompt('enter space ammount in MB:');
-            var typeBool = prompt('enter type (0 for initial, 1 for upgrade):');
-            type = (typeBool ? "upgrade" : "initial");
-            db.codes.insert({ code: code, space: parseFloat(space), type: type, active: true }, {});
-            console.log(chalk.green("DONE!"));
+            var space = prompt('enter space ammount in GB:');
+            var typeBool = prompt('enter type (0 for registration code, 1 for upgrade code):');
+            type = (typeBool ? "upgrade" : "reg");
+            db.codes.insert({ code: code, space: parseFloat(space) * 1000, type: type, benefit: 0, active: true }, function(err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            console.log(chalk.green("DONE! Code ") + chalk.yellow(code) + chalk.green(" inserted!"));
             break;
         case "2":
+            var code = prompt('enter code:');
+            var typeBool = prompt('enter type (0 for registration code, 1 for upgrade code):');
+            type = (typeBool ? "upgrade" : "reg");
+            db.codes.insert({ code: code, space: null, type: type, benefit: 1, active: true }, function(err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            console.log(chalk.green("DONE! Code ") + chalk.yellow(code) + chalk.green(" inserted!"));
+            break;
+        case "3":
             var code = prompt('enter code:');
             db.codes.remove({ code: code }, {});
             console.log(chalk.green("DONE!"));
             break;
-        case "3":
+        case "4":
             var code = prompt('enter code:');
-            var active = prompt('enter state(0 for inactive, 1 for active):');
+            var active = prompt('enter state (0 for inactive, 1 for active):');
             db.codes.update({ code: code }, { $set: { active: active } }, {});
             console.log(chalk.green("DONE!"));
             break;
         case "":
-            end = true;
             console.log(chalk.green("BYE!"));
+            break mainLoop;
             break;
         default:
             break;
