3a9773cb1f4e6114d76bc15ad3f9b29fb57f410b
Matas R <matas2k@gmail.com>
2017-12-31 18:54:21 +0200

    moved server.js

--- src/server.js
+++ src/server.js
@@ -22,7 +22,6 @@ const du = require('du');
 //isemu - ir _ is generatoriaus, nes nuxtjs dynamic routing sistemai nepatinka jie
 shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
 
-
 //uzkraunam DB
 db = {};
 db.users = new Datastore({
