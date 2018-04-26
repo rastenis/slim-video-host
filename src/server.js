deeee34d0369f680b5c57a15388bd76a36b1e021
Matas R <matas2k@gmail.com>
2018-04-26 16:37:04 +0300

    http to https for email token link

--- src/server.js
+++ src/server.js
@@ -312,7 +312,7 @@ app.post('/api/requestReset', function(req, res) {
                 subject: 'Password Reset',
                 text: 'You are receiving this because a password reset for your account was requested.\n\n' +
                     'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                    'http://' + req.headers.host + '/r/' + token + '\n\n' +
+                    'https://' + req.headers.host + '/r/' + token + '\n\n' +
                     'If you did not request this, please ignore this email and your password will remain unchanged.\n\n'
             };
             nmlTrans.sendMail(mailOptions, function(err) {
