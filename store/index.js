586c42dda8cada2bab4805f9cde7edfa871327b9
Scharkee <matas2k@gmail.com>
2019-08-03 11:44:47 +0300

    package updates

--- store/index.js
+++ store/index.js
@@ -80,7 +80,6 @@ const store = () => new Vuex.Store({
                 })
                 .then((authUser) => {
                     commit('SET_USER', authUser);
-
                 })
         },
         upload({
