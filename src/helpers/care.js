7750ae70840a3c33e88e71c3c32beaa1a83a91e4
Scharkee <matas2k@gmail.com>
2019-08-03 22:49:49 +0300

    maintenance refractor

--- src/helpers/care.js
+++ src/helpers/care.js
@@ -1,9 +1,9 @@
 const chalk = require("chalk");
 const prompt = require("prompt-sync")({});
 const fs = require("fs-extra");
-const async = require("async");
+const path = require("path");
 
-const config = require("../../config");
+const config = require(path.resolve("config.json"));
 
 mainLoop: while (true) {
   console.log(" ");
@@ -16,83 +16,52 @@ mainLoop: while (true) {
 
   switch (choice) {
     case "1":
-      require("../external/maintenance.js").preLaunch(config);
+      require(path.resolve("src", "external", "maintenance.js")).preLaunch(
+        config
+      );
       console.log(chalk.green("DONE! Maintenance launched!"));
       break;
     case "2":
-      let conf = prompt("Are you sure? This is irreversible!(1=yes,0=no):");
-      if (conf === "1") {
-        async.waterfall(
-          [
-            function(done) {
-              //removing all videos + thumbnails
-              fs.remove(config.storagePath, err => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log("Wiped videos + thumbnails...");
-                }
-                done();
-              });
-            },
-            function(done) {
-              fs.remove("./db", err => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log("Wiped databases...");
-                }
-                done();
-              });
-            },
-            function(done) {
-              fs.remove("./.nuxt", err => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log("Wiped nuxt build...");
-                }
-                done();
-              });
-            },
-            function(done) {
-              fs.remove("./tmp", err => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log("Wiped certs...");
-                }
-                done();
-              });
-            },
-            function(done) {
-              fs.remove("config.json", err => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log("Wiped configuration...");
-                }
-                done();
-              });
-            }
-          ],
-          function(err) {
-            if (err) {
-              console.log(err);
-            }
-            console.log("FINISHED! Leaving maintenance....");
-            leave();
-          }
-        );
-        break mainLoop;
+      let conf =
+        prompt(
+          "Are you sure? This is irreversible! (y/N):",
+          "N"
+        ).toUpperCase() == "Y";
+      if (!conf) {
+        break;
       }
-      break;
+      // removing everything
+      fs.remove(path.resolve(config.storagePath))
+        .then(() => {
+          console.log("Wiped videos + thumbnails...");
+          return fs.remove(path.resolve(config.dbPath));
+        })
+        .then(() => {
+          console.log("Wiped databases...");
+          return fs.remove(path.resolve(".nuxt"));
+        })
+        .then(() => {
+          console.log("Wiped nuxt build data...");
+          return fs.remove(path.resolve("tmp"));
+        })
+        .then(() => {
+          console.log("Wiped certs...");
+          return fs.remove(path.resolve("config.json"));
+        })
+        .then(() => {
+          console.log("Wiped configuraton...");
+          console.log("FINISHED! Leaving maintenance....");
+          leave();
+        })
+        .catch(e => {
+          console.log(e);
+        });
+      break mainLoop;
     case "":
       console.log("finishing up....");
       break mainLoop;
       leave();
       break;
-
     default:
       break;
   }
